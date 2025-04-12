540228400614ac2aeca5b19f217283e4af69f918
Santhosh Vodnala <Santhosh.Vodnala@iiitb.ac.in>
2025-04-12 11:36:43 +0530

    Promises object

---
+++ Promises/index.js
@@ -0,0 +1,57 @@
+// Tasks:
+// 1. Walk the dog
+// 2. Clean the house
+// 3. Do the laundry
+
+function walkDog() {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const dogWalked = true;
+
+            if (dogWalked) {
+                resolve("Dog walked");
+            } else {
+                reject("Dog NOT walked");
+            }
+        }, 1500);
+    });
+}
+
+function cleanHouse() {
+    
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            const houseCleaned = true;
+
+            if (houseCleaned) {
+                resolve("House cleaned");
+            }
+            else {
+                reject("House NOT cleaned");
+            }
+        }, 500);
+    })
+}
+
+function doLaundry() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const laundryDone = true;
+
+            if (laundryDone) {
+                resolve("Laundry done");
+            } else {
+                reject("Laundry NOT done");
+            }
+        }, 2000);
+    });
+}
+
+
+walkDog().then(value => {console.log(value); return cleanHouse();})
+         .then(value => {console.log(value); return doLaundry();})
+         .then(value => {console.log(value); console.log("All tasks completed");})
+         .catch(error => console.log(error))
